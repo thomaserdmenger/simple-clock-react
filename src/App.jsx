0e3bc51e0143c03504128ad9b48688008b668642
thomaserdmenger <thomas.erdmenger@googlemail.com>
2023-11-16 09:03:55 +0100

    Add clock component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,17 @@
-export default function App() {}
+import * as React from 'react'
+
+export default function App() {
+  const [time, setTime] = React.useState(null)
+
+  React.useEffect(() => {
+    const newTime = setInterval(() => {
+      setTime(new Date().toLocaleTimeString())
+    }, 1000)
+
+    return () => {
+      clearInterval(newTime)
+    }
+  }, [])
+
+  return <p>{time}</p>
+}
